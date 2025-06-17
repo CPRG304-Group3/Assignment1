4deee116b3a1f8a447ac780ea2934d423f03248f
Chioma <96903876+chiomaeme@users.noreply.github.com>
2025-06-17 10:14:57 -0600

    shapes can be loaded from text file

--- src/appDomain/AppDriver.java
+++ src/appDomain/AppDriver.java
@@ -2,9 +2,7 @@ package appDomain;
 
 import shapes.*;
 import utilities.GeometricShapeADT;
-
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class AppDriver
@@ -16,19 +14,20 @@ public class AppDriver
 		String filename = "";
 		char compareBy;
 		char sortingAlgorithm;
-		int arraySize;
 
+		// Validate arguments passed with program needs
 		if (args.length != 3){
 			System.out.println("You need to pass 3 arguments (T, F & S)");
 			System.exit(0);
 		}
 
+		// Validate that values are passed with each argument
 		if (args[0].length() <= 3 || args[1].length() <= 2 || args[2].length() <= 2){
 			System.out.println("You need to pass data with each argument");
 			System.exit(1);
 		}
 
-		// Switch cases that store the sort method to use in the program based on the argument given
+		// Store the choice given by user for program
 		for (String argument: args) {
 			if (argument.startsWith("-t") || argument.startsWith("-T")) {
 				compareBy = argument.charAt(2);
@@ -42,54 +41,56 @@ public class AppDriver
 			}
 		}
 
+		try (BufferedReader input = new BufferedReader(new FileReader(filename))) {
+			String line;
+			int arraySize = 0;
+			int counter = 0;
 
-		try (Scanner input = new Scanner(new File(filename))) {
-			if (input.hasNextInt()) {
-				arraySize = input.nextInt();
-				GeometricShapeADT[] geometricShape = new GeometricShapeADT[arraySize];
-				int counter = 0;
-				while (input.hasNextLine()) {
-					String shape = input.next();
-					double side1 = input.nextDouble();
-					double side2 = input.nextDouble();
+			GeometricShapeADT[] allShapes = new GeometricShapeADT[Integer.parseInt(input.readLine())];
+
+			while ((line = input.readLine()) != null) {
+				String[] lineValues = line.split(" ");
+				if (lineValues.length == 3) {
+					String shape = lineValues[0].trim();
+					double height = Double.parseDouble(lineValues[1].trim());
+					double side = Double.parseDouble(lineValues[2].trim());
 					switch (shape) {
 						case "Cone":
-							geometricShape[counter] = new Cone(side1, side2);
+							allShapes[counter] = new Cone(height, side);
 							break;
 						case "Cylinder":
-							geometricShape[counter] = new Cylinder(side1, side2);
+							allShapes[counter] = new Cylinder(height, side);
 							break;
 						case "Pyramid":
-							geometricShape[counter] = new Pyramid(side1, side2);
+							allShapes[counter] = new Pyramid(height, side);
 							break;
 						case "OctagonalPrism":
-							geometricShape[counter] = new OctagonalPrism(side1, side2);
+							allShapes[counter] = new OctagonalPrism(height, side);
 							break;
 						case "PentagonalPrism":
-							geometricShape[counter] = new PentagonalPrism(side1, side2);
+							allShapes[counter] = new PentagonalPrism(height, side);
 							break;
 						case "SquarePrism":
-							geometricShape[counter] = new SquarePrism(side1, side2);
+							allShapes[counter] = new SquarePrism(height, side);
 							break;
 						case "TriangularPrism":
-							geometricShape[counter] = new TriangularPrism(side1, side2);
+							allShapes[counter] = new TriangularPrism(height, side);
 							break;
-						default :
+						default:
 							System.out.println("You have entered an unsupported argument for this program");
 							break;
 					}
-					counter++;
 				}
-				for (GeometricShapeADT shape : geometricShape) {
-					System.out.println(shape);
-				}
+				counter++;
 			}
-		} catch (FileNotFoundException e){
-			System.out.println("File not found");
-			System.exit(5);
+//			for (GeometricShapeADT shape: allShapes) {
+//				System.out.println(shape);
+//			}
+		} catch (IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
 		}
 
-		// Based on
+
 	}
 
 }
